10961028ba41252d4fee65044d78b944679eef6b
IFX-Anusha <Anusha.TR@infineon.com>
2026-04-28 15:01:19 +0530

    psoc-edge: Add lwIP socket module support.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- ports/psoc-edge/mpconfigport.h
+++ ports/psoc-edge/mpconfigport.h
@@ -87,6 +87,11 @@
 #define MICROPY_PY_SYS_PLATFORM                 "psoc-edge"
 #define MICROPY_PY_SYS_EXC_INFO                 (1)
 
+// Enable errno module for socket error handling
+#define MICROPY_PY_ERRNO                        (1)
+// Use internal errno values (matches lwIP errno numbering used by sockets).
+#define MICROPY_USE_INTERNAL_ERRNO              (1)
+
 // Float and Math support
 #define MICROPY_PY_BUILTINS_FLOAT               (1)
 #define MICROPY_PY_MATH                         (1)
