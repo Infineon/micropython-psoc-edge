99fd7b3aa72abb9994dc4d3a3556547306fc09b4
AkshayChandra-IFX <akshaychandra.nagzarkar@infineon.com>
2026-08-04 04:22:28 +0000

    psoc-edge/adc: Guard ADC deinit.
    Signed-off-by: AkshayChandra-IFX <akshaychandra.nagzarkar@infineon.com>

--- ports/psoc-edge/modmachine.c
+++ ports/psoc-edge/modmachine.c
@@ -111,7 +111,9 @@ void machine_deinit(void) {
     #if MICROPY_PY_MACHINE_SPI_TARGET
     machine_spi_target_deinit_all();
     #endif
+    #if MICROPY_PY_MACHINE_ADC
     machine_adc_deinit_all();
+    #endif
     machine_pdm_pcm_deinit_all();
     machine_ipc_deinit_all();
     machine_counter_deinit_all();
